05250f6ff66b71e06846e1fab5a795f72e24241c
KIARA SMALL <SMALLKAY1726@GMAIL.COM>
2021-08-31 23:08:20 -0500

    commit on main

---
+++ assignment/main.js
@@ -0,0 +1,14 @@
+//console.log("love at first sight")
+
+
+function myFunction() 
+{
+
+    let txtBox = document.createElement("INPUT");
+    txtBox.setAttribute("type", "text");
+    txtBox.setAttribute("value", "Hello World!");
+    document.body.appendChild(txtBox);
+   
+}
+// let codeCrewString = ["Code School is a hands-on class designed to train individuals to be entry-level software developers within a six-month course. The minimum education requirement is at least a high school diploma or GED. Students work in a small classroom setting and use real-word technologies to learn the fundamentals of coding, app development, the leadership, and the life skills needed for a successful career and job market competitiveness."]
+//   codeCrewString.replace(codeCrewString);
